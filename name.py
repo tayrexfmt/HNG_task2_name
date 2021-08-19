2829fda65d3f771a01c677e7ebeedbb06c3c618e
tayrexfmt <tayrexfmt@gmail.com>
2021-08-19 11:26:32 +0100

    HNGi8_task2_name

---
+++ name.py
@@ -0,0 +1,12 @@
+class HngName():
+    def __init__(self):
+        self.first_name = 'Oluwafemi'
+        self.middle_name = 'Timothy'
+        self.last_name = 'Oladosu'
+
+
+fullname = HngName()
+print(f'{fullname.first_name} {fullname.middle_name}, {fullname.last_name.upper()}')
+
+        
+
